5f0ad7ec61868e07d95cb167c7aae79c954a5825
濤 <btao.cn@gmail.com>
2016-07-26 15:23:45 +0800

    adding token handle logic before send http request.

--- kanban/foundation/global-settings.js
+++ kanban/foundation/global-settings.js
@@ -1,30 +1,23 @@
 /**
  * Created by xubt on 6/18/16.
  */
+kanbanApp.romote_entrance = "http://localhost:8080/entrance";
 
 kanbanApp.run(function (editableOptions, localStorageService) {
     editableOptions.theme = 'bs3';
     localStorageService.set("userId", "341182");
     localStorageService.set("register", "341182");
 });
-kanbanApp.factory('httpInterceptor', ['$q', '$injector', function ($q, $injector) {
+kanbanApp.factory('httpInterceptor', ['$q', '$injector', 'localStorageService', function ($q, $injector, localStorageService) {
     var isHasHttpError = false;
     return {
-        // optional method
         'request': function (config) {
-            // do something on success
+            var token = localStorageService.get("identity.token");
+            var userName = localStorageService.get("identity.userName");
+            config.headers.token = token;
+            config.headers.userName = userName;
             return config;
         },
-
-        // optional method
-        'requestError': function (rejection) {
-            // do something on error
-            if (canRecover(rejection)) {
-                return responseOrNewPromise;
-            }
-            return $q.reject(rejection);
-        },
-
 
         // optional method
         'response': function (response) {
@@ -43,12 +36,13 @@ kanbanApp.factory('httpInterceptor', ['$q', '$injector', function ($q, $injector
                 animation: true,
                 templateUrl: 'foundation/modal/partials/error-dialog.html',
                 controller: function ($scope, $uibModalInstance) {
-                    $scope.title = '连接错误';
+                    $scope.title = '错误';
                     if (rejection.status === -1) {
                         $scope.message = "网络错误,请确认远程服务器运行正常。";
                     }
                     if (rejection.status === 500) {
-                        $scope.message = "500-远程服务器内部发生错误。";
+                        $scope.title = "500-远程服务器内部错误";
+                        $scope.message = rejection.data.message;
                     }
                     if (rejection.status === 400) {
                         $scope.title = '400-参数不合法';
@@ -56,6 +50,10 @@ kanbanApp.factory('httpInterceptor', ['$q', '$injector', function ($q, $injector
                     }
                     if (rejection.status === 401) {
                         $scope.title = '401- 访问受限';
+                        $scope.message = rejection.data.message;
+                    }
+                    if (rejection.status === 409) {
+                        $scope.title = '409-冲突';
                         $scope.message = rejection.data.message;
                     }
                     $scope.ok = function () {
