57aed3083f1b47c65ae6c4c166c550e566869d2a
濤 <btao.cn@gmail.com>
2016-07-26 15:27:58 +0800

    refactoring codes.

--- kanban/component/board/services/boardsService.js
+++ kanban/component/board/services/boardsService.js
@@ -1,21 +1,15 @@
 /**
  * Created by xubt on 5/26/16.
  */
-kanbanApp.factory('boardsService', ['$http', '$q',
-    function ($http, $q) {
+kanbanApp.factory('boardsService', ['$http', '$q', 'httpServices',
+    function ($http, $q, httpServices) {
         return {
             boardsLink: '',
             load: function (_boardsLink) {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-                $http({
+                return httpServices.send({
                     method: 'GET',
                     url: _boardsLink
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
                 });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
             },
             loadBoardByLink: function (_boardLink) {
                 var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
@@ -30,22 +24,12 @@ kanbanApp.factory('boardsService', ['$http', '$q',
                 return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
             },
             create: function (_board) {
-                var deferred = $q.defer();
-                $http({
+                return httpServices.send({
                     method: 'POST',
                     contentType: 'application/json',
                     data: JSON.stringify(_board),
-                    headers: {
-                        'userId': '112'
-                    },
                     url: this.boardsLink
-                }).success(function (data, status, headers, config) {
-                    console.log(data);
-                    deferred.resolve(data);
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);
                 });
-                return deferred.promise;
             }
         };
     }]);
