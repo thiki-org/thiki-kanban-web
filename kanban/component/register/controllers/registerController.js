dcf7d5123e05e835afda3fb4669efc4ee5b89664
濤 <btao.cn@gmail.com>
2016-08-04 14:21:19 +0800

    optimizing alert window.

--- kanban/component/register/controllers/registerController.js
+++ kanban/component/register/controllers/registerController.js
@@ -2,8 +2,8 @@
  * Created by xubt on 4/20/16.
  */
 
-kanbanApp.controller('registerController', ['$scope', '$location', '$q', 'publicKeyServices', 'registrationService', 'localStorageService', '$uibModalInstance',
-    function ($scope, $location, $q, publicKeyServices, registrationService, localStorageService, $uibModalInstance) {
+kanbanApp.controller('registerController', ['$scope', '$location', '$q', 'publicKeyServices', 'registrationService', 'localStorageService', '$uibModalInstance', '$uibModal',
+    function ($scope, $location, $q, publicKeyServices, registrationService, localStorageService, $uibModalInstance, $uibModal) {
         $scope.title = "注册";
         $scope.register = function () {
             var publicKeyPromise = publicKeyServices.loadPublicKey(localStorageService.get("publicKeyLink"));
@@ -20,7 +20,18 @@ kanbanApp.controller('registerController', ['$scope', '$location', '$q', 'public
                 var registrationLink = _data._links.registration.href;
                 var registrationPromise = registrationService.register(registration, registrationLink);
                 registrationPromise.then(function (_data) {
-                    alert("register success!");
+                    $uibModal.open({
+                        animation: true,
+                        templateUrl: 'foundation/modal/partials/notice-dialog.html',
+                        controller: function ($scope, $uibModalInstance) {
+                            $scope.title = '注册成功';
+                            $scope.message = "账号 " + _data.email + " 已经注册成功。";
+                            $scope.ok = function () {
+                                $uibModalInstance.close();
+                            };
+                        },
+                        size: 'sm'
+                    });
                     $uibModalInstance.dismiss('cancel');
                 });
             });
