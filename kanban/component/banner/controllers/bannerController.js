8708032e324ac14048c062d97b707973eb71c434
濤 <btao.cn@gmail.com>
2016-08-05 09:59:13 +0800

    adding aside relative.

--- kanban/component/banner/controllers/bannerController.js
+++ kanban/component/banner/controllers/bannerController.js
@@ -2,7 +2,11 @@
  * Created by xubt on 4/20/16.
  */
 
-kanbanApp.controller('bannerController', ['$scope',
-    function ($scope) {
+kanbanApp.controller('bannerController', ['$scope', '$aside', 'localStorageService',
+    function ($scope, $aside, localStorageService) {
         $scope.isShowBanner = true;
+        $scope.aside = {
+            "title": localStorageService.get("identity.userName"),
+            "content": "内容"
+        };
     }]);
