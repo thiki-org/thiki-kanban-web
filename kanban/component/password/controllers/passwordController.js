d6b1aa04bb65b903152c4b7ca4168e93426e4439
濤 <btao.cn@gmail.com>
2016-08-19 18:46:54 +0800

    Optimizing handle logic.

--- kanban/component/password/controllers/passwordController.js
+++ kanban/component/password/controllers/passwordController.js
@@ -2,10 +2,11 @@
  * Created by xubt on 4/20/16.
  */
 
-kanbanApp.controller('passwordController', ['$scope', '$location', '$q', 'passwordService', '$interval', 'localStorageService', '$uibModalInstance',
-    function ($scope, $location, $q, passwordService, $interval, localStorageService, $uibModalInstance) {
+kanbanApp.controller('passwordController', ['$scope', '$location', '$q', 'passwordService', '$interval', 'localStorageService', '$uibModalInstance', '$uibModal',
+    function ($scope, $location, $q, passwordService, $interval, localStorageService, $uibModalInstance, $uibModal) {
         $scope.title = "找回密码";
         $scope.sendVerificationCodeButtonText = "发送验证码";
+        var passwordResetApplicationLink;
         $scope.sendPasswordRetrievalApplication = function () {
             $scope.isDisableSendVerificationCodeButton = true;
             $scope.sendVerificationCodeButtonText = "发送中...";
@@ -13,10 +14,10 @@ kanbanApp.controller('passwordController', ['$scope', '$location', '$q', 'passwo
             var passwordRetrievalApplicationLink = localStorageService.get("entranceData")._links.passwordRetrievalApplication.href;
             var retrievalApplicationPromise = passwordService.retrievalApplication(passwordRetrievalApplicationLink, retrievalApplication);
             retrievalApplicationPromise.then(function (_data) {
-
+                passwordResetApplicationLink = _data._links.passwordResetApplication.href;
                 var count = 60;
                 $scope.timer = $interval(function () {
-                    if (count == 0) {
+                    if (count === 0) {
                         $interval.cancel($scope.timer);
                         $scope.isDisableSendVerificationCodeButton = false;
                         $scope.sendVerificationCodeButtonText = "发送验证码";
@@ -27,25 +28,23 @@ kanbanApp.controller('passwordController', ['$scope', '$location', '$q', 'passwo
                     }
                     count--;
                 }, 1000);
+            }, function () {
+                $scope.isDisableSendVerificationCodeButton = false;
+                $scope.sendVerificationCodeButtonText = "发送验证码";
             });
+
         };
-        $scope.cancel = function () {
-            $uibModalInstance.dismiss('cancel');
-        };
-        $scope.keyPress = function ($event) {
-            if ($scope.loginForm.$invalid) {
-                return;
-            }
-            if ($event.keyCode == 13) {
-                $scope.login();
-            }
-        };
-        $scope.openForgetPasswordModal = function () {
-            $uibModal.open({
-                animation: true,
-                templateUrl: 'component/password/partials/forget-password.html',
-                controller: "loginController",
-                size: 'sm'
+        $scope.applyReset = function () {
+            var resetApplication = {email: $scope.email, verificationCode: $scope.verificationCode};
+            var resetPromise = passwordService.applyReset(passwordResetApplicationLink, resetApplication);
+            resetPromise.then(function (_data) {
+                $uibModalInstance.dismiss('cancel');
+                $uibModal.open({
+                    animation: true,
+                    templateUrl: 'component/password/partials/password-reset.html',
+                    controller: "passwordController",
+                    size: 'sm'
+                });
             });
         };
     }]);
