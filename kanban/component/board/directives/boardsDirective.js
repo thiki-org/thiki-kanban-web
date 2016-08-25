200c77296ac57d9e2c6928739c54fa1d0d9559bb
濤 <btao.cn@gmail.com>
2016-08-25 14:40:47 +0800

    Adding updateBoard and deleteBoard logic.

--- kanban/component/board/directives/boardsDirective.js
+++ kanban/component/board/directives/boardsDirective.js
@@ -7,7 +7,7 @@ kanbanApp.directive('boardBanner', function () {
         restrict: 'E',
         templateUrl: 'component/board/partials/board-banner.html',
         replace: true,
-        controller: ['$scope', '$location', 'boardsService', 'localStorageService', function ($scope, $location, boardsService, localStorageService) {
+        controller: ['$scope', '$location', 'boardsService', 'localStorageService', '$uibModal', function ($scope, $location, boardsService, localStorageService, $uibModal) {
             var boardLink = localStorageService.get("boardLink");
             var boardsLink = localStorageService.get("identity.userName") + '/boards';
 
@@ -18,6 +18,44 @@ kanbanApp.directive('boardBanner', function () {
             $scope.toBoards = function () {
                 $location.path(boardsLink);
             };
+            $scope.updateBoard = function (_name, _board) {
+                var board = _board;
+                if (_name === "") {
+                    return "请输入看板名称";
+                }
+                board.name = _name;
+                boardsService.update(board);
+            };
+            $scope.mouseover = function () {
+                $scope.isDisplaySetting = true;
+            };
+
+            $scope.onMouseLeave = function () {
+                $scope.isDisplaySetting = false;
+            };
+
+            $scope.deleteBoard = function (_board) {
+                var thisScope = $scope;
+                $uibModal.open({
+                    animation: false,
+                    templateUrl: 'component/board/partials/delete-board-confirm.html',
+                    controller: function ($scope, $uibModalInstance) {
+                        $scope.title = '请输入看板名称以确认删除';
+                        $scope.board = _board;
+                        $scope.ok = function () {
+                            var boardPromise = boardsService.deleteBoard(_board);
+                            boardPromise.then(function (_data) {
+                                thisScope.toBoards();
+                            });
+                            $uibModalInstance.close();
+                        };
+                        $scope.cancel = function () {
+                            $uibModalInstance.dismiss('cancel');
+                        };
+                    },
+                    size: 'sm'
+                });
+            }
         }]
     };
 });
