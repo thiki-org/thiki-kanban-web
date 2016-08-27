a3dcac1db118e0ef929fe10577798b9e47d2ade2
濤 <btao.cn@gmail.com>
2016-08-27 14:41:33 +0800

    Refactoring create code.

--- kanban/component/card/services/cardsService.js
+++ kanban/component/card/services/cardsService.js
@@ -1,6 +1,6 @@
 /* Services */
 
-kanbanApp.factory('cardsServices', ['$http', '$q', function ($http, $q) {
+kanbanApp.factory('cardsServices', ['$http', '$q', 'httpServices', function ($http, $q, httpServices) {
     return {
         loadCardsByProcedureId: function (cardsUrl) {
             var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
@@ -19,23 +19,7 @@ kanbanApp.factory('cardsServices', ['$http', '$q', function ($http, $q) {
             return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
         },
         create: function (_card, _procedureCardsUrl) {
-            var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-
-            $http({
-                method: 'POST',
-                contentType: 'application/json',
-                data: JSON.stringify(_card),
-                headers: {
-                    'userId': '112'
-                },
-                url: _procedureCardsUrl
-            }).success(function (data, status, headers, config) {
-                console.log(data);
-                deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);   // 声明执行失败，即服务器返回错误
-            });
-            return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+            return httpServices.post(_card, _procedureCardsUrl);
         },
         update: function (_card) {
             var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
