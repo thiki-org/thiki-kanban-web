f87ab31483d6d97ed9874e40026d1adf74252398
濤 <btao.cn@gmail.com>
2016-08-28 10:38:38 +0800

    Checking token before loading.

--- kanban/component/banner/controllers/bannerController.js
+++ kanban/component/banner/controllers/bannerController.js
@@ -2,11 +2,17 @@
  * Created by xubt on 4/20/16.
  */
 
-kanbanApp.controller('bannerController', ['$scope', '$aside', 'localStorageService',
-    function ($scope, $aside, localStorageService) {
+kanbanApp.controller('bannerController', ['$scope', '$aside', 'localStorageService', '$location', '$uibModal',
+    function ($scope, $aside, localStorageService, $location, $uibModal) {
         $scope.isShowBanner = true;
         $scope.aside = {
             "title": localStorageService.get("identity.userName"),
             "content": "内容"
         };
-    }]);
+        $scope.init = function () {
+            if (localStorageService.get("identity.token") === null) {
+                $location.path("/welcome");
+            }
+        };
+    }])
+;
