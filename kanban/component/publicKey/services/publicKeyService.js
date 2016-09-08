3422ce849946c6cd106e7edf54d8be91d7c53fb1
濤 <btao.cn@gmail.com>
2016-09-08 08:30:34 +0800

    Introducing httpService.

--- kanban/component/publicKey/services/publicKeyService.js
+++ kanban/component/publicKey/services/publicKeyService.js
@@ -1,19 +1,9 @@
 /* Services */
 
-kanbanApp.factory('publicKeyServices', ['$http', '$q', function ($http, $q) {
+kanbanApp.factory('publicKeyServices', ['$http', '$q', 'httpServices', function ($http, $q, httpServices) {
     return {
         loadPublicKey: function (_publicKeyLink) {
-            var deferred = $q.defer();
-            $http({
-                method: 'GET',
-                url: _publicKeyLink
-            }).success(function (data, status, headers, config) {
-                tasks = data;
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
+            return httpServices.get(_publicKeyLink);
         }
     };
 }]);
