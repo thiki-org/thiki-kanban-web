bf3194a705037bfe9c2ed1383cbc5f270f98f490
濤 <btao.cn@gmail.com>
2016-09-09 08:20:04 +0800

    Introducing httpService.

--- kanban/component/board/services/boardsService.js
+++ kanban/component/board/services/boardsService.js
@@ -6,44 +6,19 @@ kanbanApp.factory('boardsService', ['$http', '$q', 'httpServices',
         return {
             boardsLink: '',
             load: function (_boardsLink) {
-                return httpServices.send({
-                    method: 'GET',
-                    url: _boardsLink
-                });
+                return httpServices.get(_boardsLink);
             },
             loadBoardByLink: function (_boardLink) {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-                $http({
-                    method: 'GET',
-                    url: _boardLink
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
-                });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+                return httpServices.get(_boardLink);
             },
             create: function (_board) {
-                return httpServices.send({
-                    method: 'POST',
-                    contentType: 'application/json',
-                    data: JSON.stringify(_board),
-                    url: this.boardsLink
-                });
+                return httpServices.post(_board, this.boardsLink);
             },
             update: function (_board) {
-                return httpServices.send({
-                    method: 'PUT',
-                    contentType: 'application/json',
-                    data: JSON.stringify(_board),
-                    url: _board._links.self.href
-                });
+                return httpServices.put(_board, _board._links.self.href);
             },
             deleteBoard: function (_board) {
-                return httpServices.send({
-                    method: 'DELETE',
-                    url: _board._links.self.href
-                });
+                return httpServices.post(_board, _board._links.self.href);
             }
         };
     }]);
