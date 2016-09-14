89487873a3af378a656ac3c1fd36d2d98c020c66
濤 <btao.cn@gmail.com>
2016-09-14 08:25:53 +0800

    Optimizing invite method.

--- kanban/component/team/controllers/teamController.js
+++ kanban/component/team/controllers/teamController.js
@@ -6,6 +6,8 @@ kanbanApp.controller('teamController', ['$scope', '$location', '$q', 'teamsServi
     function ($scope, $location, $q, teamsService, localStorageService, $uibModal) {
         var teamLink = localStorageService.get("teamLink");
         var teamPromise = teamsService.loadTeamByLink(teamLink);
+        $scope.isDisableInvitationButton = false;
+        $scope.invitationButtonText = "邀请";
         teamPromise.then(function (_team) {
             $scope.team = _team;
 
@@ -14,7 +16,29 @@ kanbanApp.controller('teamController', ['$scope', '$location', '$q', 'teamsServi
             var membersPromise = teamsService.loadMembers(membersLink);
 
             membersPromise.then(function (_data) {
-                $scope.members = _data;
+                $scope.members = _data.members;
+                $scope.invitationLink = _data._links.invitation.href;
+
             });
+            $scope.openInvitationForm = function () {
+                $uibModal.open({
+                    animation: true,
+                    templateUrl: 'component/team/partials/member-invitation.html',
+                    controller: "teamController",
+                    size: 'sm'
+                });
+            };
+
+            $scope.invite = function () {
+                $scope.invitationButtonText = "邀请中..";
+                $scope.isDisableInvitationButton = true;
+                var invitation = {invitee: $scope.invitee};
+                var invitationPromise = teamsService.invite(invitation, $scope.invitationLink);
+                invitationPromise.then(function (_data) {
+                }).finally(function () {
+                    $scope.invitationButtonText = "邀请";
+                    $scope.isDisableInvitationButton = false;
+                });
+            }
         });
     }]);
