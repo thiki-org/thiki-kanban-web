6f69266c9f4005a2a35c7c21a3fd3b5d26c260d0
濤 <btao.cn@gmail.com>
2016-09-20 17:29:46 +0800

    Removing unused code.

--- kanban/component/banner/controllers/bannerController.js
+++ kanban/component/banner/controllers/bannerController.js
@@ -2,18 +2,12 @@
  * Created by xubt on 4/20/16.
  */
 
-kanbanApp.controller('bannerController', ['$scope', '$aside', 'localStorageService', '$location', '$uibModal',
-    function ($scope, $aside, localStorageService, $location, $uibModal) {
+kanbanApp.controller('bannerController', ['$scope', '$aside', 'localStorageService', '$location',
+    function ($scope, $aside, localStorageService, $location) {
         $scope.isShowBanner = true;
-        $scope.aside = {
-            "title": localStorageService.get("identity.userName"),
-            "content": "内容"
-        };
-        $scope.userName = localStorageService.get("identity.userName");
         $scope.init = function () {
             if (localStorageService.get("identity.token") === null) {
                 $location.path("/welcome");
             }
         };
-    }])
-;
+    }]);
