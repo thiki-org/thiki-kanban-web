0efe90785cc570fb3c5242a5da1f360acc24aed3
濤 <btao.cn@gmail.com>
2016-09-23 08:55:46 +0800

    Adding notificationService.

---
+++ kanban/component/notification/services/notificationsService.js
@@ -0,0 +1,15 @@
+/**
+ * Created by xubt on 9/20/16.
+ */
+kanbanApp.factory('notificationsService', ['$http', '$q', 'httpServices',
+    function ($http, $q, httpServices) {
+        return {
+            teamsLink: '',
+            loadUnreadNotificationsTotal: function (_loadUnreadNotificationsTotalLink) {
+                return httpServices.get(_loadUnreadNotificationsTotalLink);
+            },
+            loadNotifications: function (_notificationUrl) {
+                return httpServices.get(_notificationUrl);
+            }
+        };
+    }]);
