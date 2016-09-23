bf996110f3cfdcada95385b96511031a0a0a380e
濤 <btao.cn@gmail.com>
2016-09-23 08:52:18 +0800

    Adding notifications entrance.

--- kanban/component/users/controllers/userController.js
+++ kanban/component/users/controllers/userController.js
@@ -7,18 +7,24 @@ kanbanApp.controller('userController', ['$scope', '$location', '$q', 'publicKeyS
         $scope.userName = localStorageService.get("identity.userName");
 
         var unreadNotificationsTotalLink = usersService.getCurrentUser()._links.unreadNotificationsTotal.href;
-        $interval(function () {
+        var pollNotifications = $interval(function () {
             var notificationPromise = notificationsService.loadUnreadNotificationsTotal(unreadNotificationsTotalLink);
             notificationPromise.then(function (_data) {
                 $scope.unreadNotificationsTotal = _data.unreadNotificationsTotal;
                 $scope.isShowNotification = $scope.unreadNotificationsTotal === 0 ? false : true;
+                localStorageService.set("notificationsUrl", _data._links.notifications.href);
+            }, function () {
+                $interval.cancel(pollNotifications);
             });
         }, 5000);
-
 
         $scope.gotoTeams = function () {
             $location.path(localStorageService.get('identity.userName') + "/teams");
         };
+        $scope.openNotifications = function () {
+            $location.path(usersService.getCurrentUser().name + "/notifications");
+        };
+
         $scope.loginOut = function () {
             $uibModal.open({
                 animation: false,
