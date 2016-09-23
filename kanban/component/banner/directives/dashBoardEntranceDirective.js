b02f5c41f1f354fbb016661474bba4d32672085d
濤 <btao.cn@gmail.com>
2016-09-23 09:25:03 +0800

    Adding controller.

--- kanban/component/banner/directives/dashBoardEntranceDirective.js
+++ kanban/component/banner/directives/dashBoardEntranceDirective.js
@@ -6,6 +6,12 @@ kanbanApp.directive('dashboardEntrance', function () {
     return {
         restrict: 'E',
         templateUrl: 'component/banner/partials/dashboard-entrance.html',
-        replace: true
-    }
+        replace: true,
+        controller: ['$scope', '$location', 'boardsService', 'localStorageService', '$uibModal', function ($scope, $location, boardsService, localStorageService, $uibModal) {
+            var dashboardLink = localStorageService.get("identity.userName") + '/dashboard';
+            $scope.toDashBoard = function () {
+                $location.path(dashboardLink);
+            };
+        }]
+    };
 });
