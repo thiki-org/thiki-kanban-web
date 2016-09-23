589d8c83f6d652377edbbb1fc93218c00d1e5814
濤 <btao.cn@gmail.com>
2016-09-23 08:56:28 +0800

    Adding dashBoardEntrance directive.

---
+++ kanban/component/banner/directives/dashBoardEntranceDirective.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xubt on 5/26/16.
+ */
+
+kanbanApp.directive('dashboardEntrance', function () {
+    return {
+        restrict: 'E',
+        templateUrl: 'component/banner/partials/dashboard-entrance.html',
+        replace: true
+    }
+});
