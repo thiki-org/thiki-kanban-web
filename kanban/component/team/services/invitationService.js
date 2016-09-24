cbc57bbd50a4e69dad9a9fcf55989429c573b1fd
濤 <btao.cn@gmail.com>
2016-09-24 11:10:00 +0800

    Adding invitationService.

---
+++ kanban/component/team/services/invitationService.js
@@ -0,0 +1,14 @@
+/**
+ * Created by xubt on 9/24/16.
+ */
+kanbanApp.factory('invitationService', ['$http', '$q', 'httpServices',
+    function ($http, $q, httpServices) {
+        return {
+            loadInvitationByLink: function (_invitationLink) {
+                return httpServices.get(_invitationLink);
+            },
+            acceptInvitation: function (_invitationLink) {
+                return httpServices.put(_invitationLink);
+            }
+        };
+    }]);
