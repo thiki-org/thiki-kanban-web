5afe1b9818479984d20671f5aa493c0a518a4388
濤 <btao.cn@gmail.com>
2016-09-24 11:07:38 +0800

    Introducing timerMessageService.

--- kanban/component/notification/directives/notificationDirective.js
+++ kanban/component/notification/directives/notificationDirective.js
@@ -7,16 +7,37 @@ kanbanApp.directive('notification', function () {
         restrict: 'E',
         templateUrl: 'component/notification/partials/notification.html',
         replace: true,
-        controller: ['$scope', '$location', 'notificationsService', 'localStorageService', '$uibModal', function ($scope, $location, notificationsService, localStorageService, $uibModal) {
+        controller: ['$scope', '$location', 'notificationsService', 'invitationService', 'teamsService', 'timerMessageService', 'localStorageService', '$uibModal', function ($scope, $location, notificationsService, invitationService, teamsService, timerMessageService, localStorageService, $uibModal) {
             $scope.readNotification = function () {
                 var notificationPromise = notificationsService.loadNotificationByLink($scope.notification._links.self.href);
-                notificationPromise.then(function (_data) {
-                    $scope.notification = _data;
+                notificationPromise.then(function (_notification) {
+                    $scope.notification = _notification;
                     if ($scope.notification.type === "team-members-invitation") {
-                        $uibModal.open({
-                            animation: false,
-                            templateUrl: 'component/team/partials/accept-invitation.html',
-                            size: 'sm'
+                        var invitationPromise = invitationService.loadInvitationByLink($scope.notification.link);
+                        invitationPromise.then(function (_invitation) {
+                            if (!_invitation.isAccepted) {
+                                var teamPromise = teamsService.loadTeamByLink(_invitation._links.team.href);
+                                teamPromise.then(function (_team) {
+                                    $uibModal.open({
+                                        animation: false,
+                                        templateUrl: 'component/team/partials/accept-invitation.html',
+                                        controller: function ($scope, $uibModalInstance) {
+                                            $scope.teamName = _team.name;
+                                            $scope.ok = function () {
+                                                var invitationPromise = invitationService.acceptInvitation($scope.notification.link);
+                                                invitationPromise.then(function (_data) {
+                                                    timerMessageService.message("已经加入团队");
+                                                });
+                                                $uibModalInstance.close();
+                                            };
+                                            $scope.cancel = function () {
+                                                $uibModalInstance.dismiss('cancel');
+                                            };
+                                        },
+                                        size: 'sm'
+                                    });
+                                });
+                            }
                         });
                     }
                 });
