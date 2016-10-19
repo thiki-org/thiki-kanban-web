f113270ccae748f99be625afb6b49a02205c7220
Tao <btao.cn@gmail.com>
2016-10-19 22:38:42 +0800

    Adding acceptanceCriteria edit feature.

--- kanban/component/card/directives/acceptanceCriteriasDirective.js
+++ kanban/component/card/directives/acceptanceCriteriasDirective.js
@@ -11,26 +11,60 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
             card: '='
         },
         controller: ['$scope', 'localStorageService', 'acceptanceCriteriaService', 'cardsServices', function ($scope, localStorageService, acceptanceCriteriaService, cardsServices) {
-            $scope.acceptanceCriteriaSaveButton = "新建验收标准";
+            $scope.acceptanceCriteriaSaveButton = "保存";
             $scope.isShowAcceptanceCriteriaForm = false;
             var acceptanceCriterias = acceptanceCriteriaService.loadAcceptanceCriterias($scope.card._links.acceptanceCriterias.href);
             acceptanceCriterias.then(function (_acceptanceCriterias) {
                 $scope.acceptanceCriterias = _acceptanceCriterias.acceptanceCriterias;
+                $scope.acceptanceCriteriasSortableOptions = {
+                    connectWith: ".acceptanceCriteria",
+                    placeholder: "acceptanceCriteria-drag-placeholder",
+                    start: function (e, ui) {
+                        console.log("staring sort.");
+                        $scope.isShowDeleteArea = true;
+                    },
+                    update: function (e, ui) {
+                        console.log("updating sort.");
+                        console.log(ui);
+                        $scope.isShowDeleteArea = true;
+                    },
+                    stop: function (e, ui) {
+                        console.log("stopping sort.");
+                        $scope.isShowDeleteArea = false;
+                    }
+                };
             });
-            $scope.createAcceptanceCriteria = function () {
-                if ($scope.isShowAcceptanceCriteriaForm == false) {
-                    $scope.isShowAcceptanceCriteriaForm = true;
-                }
+
+            $scope.isShowAcceptanceCriteriaCreationButton = true;
+            $scope.isDisableAcceptanceCriteriaSaveButton = false;
+            $scope.openAcceptanceCriteriaCreationForm = function () {
+                $scope.isShowAcceptanceCriteriaForm = true;
+                $scope.isShowAcceptanceCriteriaCreationButton = false;
+            };
+
+            $scope.addAcceptanceCriteriaCreation = function () {
+
+                $scope.isShowAcceptanceCriteriaCreationButton = false;
+                $scope.isDisableAcceptanceCriteriaSaveButton = true;
+                $scope.acceptanceCriteriaSaveButton = "保存中..";
+
                 var acceptanceCriteria = {summary: $scope.acceptanceCriteriaSummary};
                 var acceptanceCriteriaPromise = acceptanceCriteriaService.create(acceptanceCriteria, $scope.card._links.acceptanceCriterias.href);
                 acceptanceCriteriaPromise.then(function (_acceptanceCriteria) {
                     $scope.acceptanceCriterias.push(_acceptanceCriteria);
                     $scope.isShowAcceptanceCriteriaForm = false;
+                    $scope.isShowAcceptanceCriteriaCreationButton = true;
+                    $scope.acceptanceCriteriaSummary = "";
+                }).finally(function () {
+                    $scope.acceptanceCriteriaSaveButton = "保存";
+                    $scope.isDisableAcceptanceCriteriaSaveButton = false;
                 });
             };
+
             $scope.cancelCreateAcceptanceCriteria = function () {
                 $scope.isShowAcceptanceCriteriaForm = false;
                 $scope.acceptanceCriteriaSummary = "";
+                $scope.isShowAcceptanceCriteriaCreationButton = true;
             }
         }]
     };
