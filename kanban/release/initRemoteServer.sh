c4769d8d4386829b12152bb802f8214551d1b72b
Tao <btao.cn@gmail.com>
2016-11-22 10:52:14 +0800

    Remove unused code.

--- kanban/release/initRemoteServer.sh
+++ kanban/release/initRemoteServer.sh
@@ -5,5 +5,5 @@ remote_entrance=$1
 echo "init remote server..."
 
 echo "kanbanApp.remote_entrance =$remote_entrance" >>kanban/release/deploy.sh
-mv src/main/resources/scripts/deploy.sh /data/thiki-kanban/sit
+
 echo "done."
