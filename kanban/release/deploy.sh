c333dd4ab70995f43d729aadef67976168844529
Tao <btao.cn@gmail.com>
2016-11-22 13:06:23 +0800

    clean deploy target directory.

--- kanban/release/deploy.sh
+++ kanban/release/deploy.sh
@@ -3,6 +3,8 @@ set -e
 artifactDirectory=$1
 deployTargetDirectory=$2
 
+echo "clean deploy target directory ..."
+rm -rf $2
 echo "copy file to deploy target directory ..."
 mv $1  $2
 echo "done."
