400e02f9ac7b3e0337cd412f442c35b2fde200f4
Tao <btao.cn@gmail.com>
2016-11-22 12:26:05 +0800

    Fix deploy scripts bug.

--- kanban/release/initRemoteServer.sh
+++ kanban/release/initRemoteServer.sh
@@ -5,7 +5,6 @@ remote_entrance=$1
 echo "init remote server..."
 echo "The remote_entrance is ${1}"
 
-echo /dev/null >>kanban/foundation/remoteServer.js
 echo "kanbanApp.remote_entrance ='${1}';" >>kanban/foundation/remoteServer.js
 
 echo "done."
