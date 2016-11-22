d2659f5330adce3c9b214c342d07705994cfda1f
Tao <btao.cn@gmail.com>
2016-11-22 11:37:07 +0800

    Fix initRemoteServer.sh bug.

--- kanban/release/initRemoteServer.sh
+++ kanban/release/initRemoteServer.sh
@@ -3,8 +3,8 @@ set -e
 remote_entrance=$1
 
 echo "init remote server..."
-echo "The remote_entrance is $1"
+echo "The remote_entrance is ${1}"
 
-echo "kanbanApp.remote_entrance =$1" >>kanban/foundation/remoteServer.js
+echo "kanbanApp.remote_entrance =${1};" >>kanban/foundation/remoteServer.js
 
 echo "done."
