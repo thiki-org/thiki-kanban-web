1e5730306ce70a2a8fc53d26c9f81d16bff91a8c
Tao <btao.cn@gmail.com>
2016-11-28 18:12:15 +0800

    Fix welcome service.

--- kanban/component/welcome/services/welcomeService.js
+++ kanban/component/welcome/services/welcomeService.js
@@ -1,19 +1,9 @@
 /* Services */
 
-kanbanApp.factory('welcomeServices', ['$http', '$q', function ($http, $q) {
+kanbanApp.factory('welcomeServices', ['$http', '$q', 'httpServices', function ($http, $q, httpServices) {
     return {
         loadEntrance: function (_entranceUrl) {
-            var deferred = $q.defer();
-            $http({
-                method: 'GET',
-                url: _entranceUrl
-            }).success(function (data, status, headers, config) {
-                tasks = data;
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
+            return httpServices.get(_entranceUrl);
         }
     };
 }]);
