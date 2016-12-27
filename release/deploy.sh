0521dcc598a8ca03a3cb9a721c2c9857dd850349
Tao <btao.cn@gmail.com>
2016-12-27 22:46:49 +0800

    Add deploy script.

---
+++ release/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+artifact=$1
+server_host=$2
+server_dir=$3
+
+echo "-> upload artifact to remote server"
+scp $artifact $server_host:$server_dir
+
+echo "-> everything is done."
