398fc224431e258e544ba23e6d76bde17beef6c4
Tao <btao.cn@gmail.com>
2017-01-04 14:08:42 +0800

    Replace $http with httpService.

--- kanban/component/login/services/loginService.js
+++ kanban/component/login/services/loginService.js
@@ -1,20 +1,9 @@
 /* Services */
 
-kanbanApp.factory('loginService', ['$http', '$q', function ($http, $q) {
+kanbanApp.factory('loginService', ['httpServices', '$q', function (httpServices, $q) {
     return {
         login: function (_loginLink, _identity, _password) {
-            var deferred = $q.defer();
-            $http({
-                method: 'get',
-                params: {identity: _identity, password: _password},
-                url: _loginLink
-            }).success(function (data, status, headers, config) {
-                tasks = data;
-                deferred.resolve(data);
-            }).error(function (data, status, headers, config) {
-                deferred.reject(data);
-            });
-            return deferred.promise;
+            return httpServices.get(_loginLink, {identity: _identity, password: _password});
         }
     };
 }]);
--- kanban/component/procedure/services/proceduresService.js
+++ kanban/component/procedure/services/proceduresService.js
@@ -1,70 +1,20 @@
 /* Services */
 
-kanbanApp.factory('proceduresServices', ['$http', '$q', 'httpServices',
-    function ($http, $q, httpServices) {
+kanbanApp.factory('proceduresServices', ['httpServices',
+    function (httpServices) {
         return {
             proceduresLink: '',
             load: function () {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-
-                // return a Promise object so that the caller can handle success/failure
-                $http({
-                    method: 'GET',
-                    dataType: 'application/json',
-                    url: this.proceduresLink
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
-                });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+                return httpServices.get(this.proceduresLink);
             },
-
             create: function (_procedure) {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-
-                // return a Promise object so that the caller can handle success/failure
-                $http({
-                    method: 'POST',
-                    contentType: 'application/json',
-                    data: JSON.stringify(_procedure),
-                    url: this.proceduresLink
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
-                });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+                return httpServices.post(_procedure, this.proceduresLink);
             },
             update: function (_procedure) {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-
-                // return a Promise object so that the caller can handle success/failure
-                $http({
-                    method: 'PUT',
-                    contentType: 'application/json',
-                    data: JSON.stringify(_procedure),
-                    url: _procedure._links.self.href
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
-                });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+                return httpServices.put(_procedure, this.proceduresLink);
             },
             deleteByLink: function (_link) {
-                var deferred = $q.defer(); // 声明延后执行，表示要去监控后面的执行
-
-                $http({
-                    method: 'DELETE',
-                    contentType: 'application/json',
-                    url: _link
-                }).success(function (data, status, headers, config) {
-                    deferred.resolve(data);  // 声明执行成功，即http请求数据成功，可以返回数据了
-                }).error(function (data, status, headers, config) {
-                    deferred.reject(data);   // 声明执行失败，即服务器返回错误
-                });
-                return deferred.promise;   // 返回承诺，这里并不是最终数据，而是访问最终数据的API
+                return httpServices.delete(_link);
             },
             resort: function (_procedures, _resortLink) {
                 return httpServices.put(_procedures, _resortLink);
