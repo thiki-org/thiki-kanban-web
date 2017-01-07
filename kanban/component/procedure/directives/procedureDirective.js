2fbb3bf00fe14e5cd94bfc1072ebfbb2e5e6fe3f
skytao <skytao1991@gmail.com>
2017-01-07 18:16:10 +0800

    Optimize procedure type display.

--- kanban/component/procedure/directives/procedureDirective.js
+++ kanban/component/procedure/directives/procedureDirective.js
@@ -65,7 +65,7 @@ kanbanApp.directive('procedure', function ($uibModal) {
                         function ($scope, teamsService, timerMessageService, $uibModalInstance, jsonService) {
                             $scope.procedure = proceduresScope.procedure;
                             $scope.procedureSaveButton = "保存";
-                            $scope.types = [{id: 0, name: "尚未开始迭代"}, {id: 1, name: "迭代计划中"}];
+                            $scope.types = [{id: 0, name: "尚未开始迭代"}, {id: 1, name: "迭代中"}];
                             $scope.statusList = [{id: 0, name: "默认"}, {id: 9, name: "完成"}];
                             $scope.types.selected = $scope.types[$scope.procedure.type];
                             $scope.statusList.selected = jsonService.findById($scope.statusList, $scope.procedure.status);
