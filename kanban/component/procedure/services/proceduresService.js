223335a040ba2b88c0421abb549152d1a29eb52f
skytao <skytao1991@gmail.com>
2017-01-07 17:58:44 +0800

    Fix some bugs.

--- kanban/component/procedure/services/proceduresService.js
+++ kanban/component/procedure/services/proceduresService.js
@@ -1,23 +1,22 @@
 /* Services */
 
-kanbanApp.factory('proceduresServices', ['httpServices',
-    function (httpServices) {
-        return {
-            proceduresLink: '',
-            load: function () {
-                return httpServices.get(this.proceduresLink);
-            },
-            create: function (_procedure) {
-                return httpServices.post(_procedure, this.proceduresLink);
-            },
-            update: function (_procedure) {
-                return httpServices.put(_procedure, _procedure._links.self.href);
-            },
-            deleteByLink: function (_link) {
-                return httpServices.delete(_link);
-            },
-            resort: function (_procedures, _resortLink) {
-                return httpServices.put(_procedures, _resortLink);
-            }
-        };
-    }]);
+kanbanApp.factory('proceduresServices', ['httpServices', function (httpServices) {
+    return {
+        proceduresLink: '',
+        load: function () {
+            return httpServices.get(this.proceduresLink);
+        },
+        create: function (_procedure) {
+            return httpServices.post(_procedure, this.proceduresLink);
+        },
+        update: function (_procedure) {
+            return httpServices.put(_procedure, _procedure._links.self.href);
+        },
+        deleteByLink: function (_link) {
+            return httpServices.delete(_link);
+        },
+        resort: function (_procedures, _resortLink) {
+            return httpServices.put(_procedures, _resortLink);
+        }
+    };
+}]);
