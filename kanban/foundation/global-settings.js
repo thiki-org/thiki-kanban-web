23cd150d50b68472193f29b4e9ab2e6f92d4cf6a
skytao <skytao1991@gmail.com>
2017-01-18 18:58:56 +0800

    Fix $rootScope.lastestOperationTime bug.

--- kanban/foundation/global-settings.js
+++ kanban/foundation/global-settings.js
@@ -20,7 +20,7 @@ kanbanApp.factory('httpInterceptor', ['$q', '$injector', 'localStorageService', 
             if (response.headers().token) {
                 localStorageService.set('identity.token', response.headers().token);
             }
-            c = moment();
+            $rootScope.lastestOperationTime = moment();
             return response;
         },
 
