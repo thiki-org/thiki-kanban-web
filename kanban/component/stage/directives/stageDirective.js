a372a797dabf76ced77604c4d1f9ecc0a7e00dad
skytao <skytao1991@gmail.com>
2017-02-19 22:52:11 +0800

    Fix stage resort bug and stage creation bug.

--- kanban/component/stage/directives/stageDirective.js
+++ kanban/component/stage/directives/stageDirective.js
@@ -2,7 +2,7 @@
  * Created by xubt on 4/29/16.
  */
 
-kanbanApp.directive('stage', function ($uibModal) {
+kanbanApp.directive('stage', function($uibModal) {
     return {
         restrict: 'E',
         templateUrl: 'component/stage/partials/stage.html',
@@ -12,132 +12,133 @@ kanbanApp.directive('stage', function ($uibModal) {
             stage: '=',
             board: '='
         },
-        controller: ['$scope', 'boardsService', 'stagesServices', function ($scope, boardsService, stagesServices, $document) {
+        controller: ['$scope', 'boardsService', 'stagesServices', function($scope, boardsService, stagesServices, $document) {
             $scope.displayStageMenu = false;
-            $scope.onStageMenuMouseOver = function ($event) {
+            $scope.onStageMenuMouseOver = function($event) {
                 $scope.displayStageMenu = true;
             };
-            $scope.onStageMenuMouseLeave = function ($event) {
+            $scope.onStageMenuMouseLeave = function($event) {
                 $scope.displayStageMenu = false;
             };
 
             var currentScope = $scope;
-            $scope.cardsCount = $scope.stage.cards.cards.length;
-            $scope.openModal = function (_message, _link) {
+            $scope.cardsCount = $scope.stage.cards === undefined ? 0 : $scope.stage.cards.cards.length;
+            $scope.openModal = function(_message, _link) {
                 $uibModal.open({
                     animation: false,
                     templateUrl: 'foundation/modal/partials/confirm-dialog.html',
-                    controller: function ($scope, $uibModalInstance) {
+                    controller: function($scope, $uibModalInstance) {
                         $scope.title = '警告';
                         $scope.message = "确定要删除" + _message + "吗?";
-                        $scope.ok = function () {
+                        $scope.ok = function() {
                             var _stageDeletePromise = stagesServices.deleteByLink(_link);
-                            _stageDeletePromise.then(function () {
+                            _stageDeletePromise.then(function() {
                                 currentScope.$parent.loadStages();
                             });
                             $uibModalInstance.close();
                         };
-                        $scope.cancel = function () {
+                        $scope.cancel = function() {
                             $uibModalInstance.dismiss('cancel');
                         };
                     },
                     size: 'sm'
                 });
             };
-            $scope.updateStage = function (_title, _stage) {
+            $scope.updateStage = function(_title, _stage) {
                 var stage = _stage;
                 if (_title === "") {
                     return "标题不能为空";
                 }
                 stage.title = _title;
                 var stagePromise = stagesServices.update(stage);
-                stagePromise.then(function () {
-                });
+                stagePromise.then(function() {});
             };
 
-            $scope.open = function () {
+            $scope.open = function() {
                 $scope.displayStageMenu = false;
             };
             var stagesScope = $scope;
-            $scope.openStageConfiguration = function () {
+            $scope.openStageConfiguration = function() {
                 $uibModal.open({
                     animation: true,
                     templateUrl: 'component/stage/partials/stage-configuration.html',
                     controller: ['$scope', 'projectsService', 'timerMessageService', '$uibModalInstance', 'jsonService',
-                        function ($scope, projectsService, timerMessageService, $uibModalInstance, jsonService) {
+                        function($scope, projectsService, timerMessageService, $uibModalInstance, jsonService) {
                             $scope.stage = stagesScope.stage;
                             $scope.stageSaveButton = "保存";
-                            $scope.types = [{id: 0, name: "迭代计划"}, {id: 1, name: "迭代中"}];
-                            $scope.statusList = [{id: 0, name: "默认"}, {id: 1, name: "处理中"}, {id: 9, name: "完成"}];
+                            $scope.types = [{ id: 0, name: "迭代计划" }, { id: 1, name: "迭代中" }];
+                            $scope.statusList = [{ id: 0, name: "默认" }, { id: 1, name: "处理中" }, { id: 9, name: "完成" }];
                             $scope.types.selected = $scope.types[$scope.stage.type];
                             $scope.statusList.selected = jsonService.findById($scope.statusList, $scope.stage.status);
-                            $scope.saveStage = function () {
+                            $scope.saveStage = function() {
                                 $scope.stage.type = $scope.types.selected.id;
                                 $scope.stage.status = $scope.statusList.selected.id;
 
                                 $scope.stageSaveButton = "保存中..";
                                 $scope.isDisableStageSaveButton = true;
                                 var stagePromise = stagesServices.update($scope.stage);
-                                stagePromise.then(function (_stage) {
+                                stagePromise.then(function(_stage) {
                                     timerMessageService.message("配置已经更新");
                                     stagesScope.stage = _stage;
                                     $scope.stage = _stage;
                                     $uibModalInstance.dismiss('cancel');
-                                }).finally(function () {
+                                }).finally(function() {
                                     $scope.stageSaveButton = "保存";
                                     $scope.isDisableStageSaveButton = false;
                                 });
                             };
-                        }],
+                        }
+                    ],
                     size: 'mid',
                     backdrop: "static"
                 });
             };
-            $scope.scrollToCardCreation = function () {
+            $scope.scrollToCardCreation = function() {
                 $scope.stage.isShowCardCreation = true;
                 var cardsElement = angular.element(document.getElementById("cards-" + $scope.stage.id));
                 var cardCreationElement = angular.element(document.getElementById("card-creation-" + $scope.stage.id));
-                cardsElement.scrollToElement(cardCreationElement, 20, 500).then(function () {
-                });
+                cardsElement.scrollToElement(cardCreationElement, 20, 500).then(function() {});
             };
-            $scope.toFullScreen = function () {
+            $scope.toFullScreen = function() {
                 $uibModal.open({
                     animation: true,
                     templateUrl: 'component/stage/partials/stage-full-screen.html',
                     controller: ['$scope', 'projectsService', 'timerMessageService', '$uibModalInstance',
-                        function ($scope, projectsService, timerMessageService, $uibModalInstance) {
+                        function($scope, projectsService, timerMessageService, $uibModalInstance) {
                             $scope.stage = stagesScope.stage;
-                            $scope.finishCardsOperation = function () {
+                            $scope.finishCardsOperation = function() {
                                 $uibModalInstance.dismiss('cancel');
                             };
-                        }],
+                        }
+                    ],
                     size: 'fullscreen'
                 });
             };
 
-            $scope.openArchiveDialog = function () {
+            $scope.openArchiveDialog = function() {
                 $uibModal.open({
                     animation: true,
                     templateUrl: 'component/stage/partials/stage-archive.html',
                     controller: ['$scope', 'projectsService', 'timerMessageService', '$uibModalInstance',
-                        function ($scope, projectsService, timerMessageService, $uibModalInstance) {
+                        function($scope, projectsService, timerMessageService, $uibModalInstance) {
                             $scope.stage = {};
                             $scope.archiveButtonText = "归档";
                             $scope.isDisableArchiveButton = false;
-                            $scope.archive = function () {
+                            $scope.archive = function() {
                                 $scope.archiveButtonText = "归档中..";
                                 $scope.isDisableArchiveButton = true;
                                 stagesServices.create($scope.stage, stagesScope.stage._links.archives.href)
-                                    .then(function () {
+                                    .then(function() {
                                         timerMessageService.message("归档成功，正在为您刷新数据..");
                                         currentScope.$parent.loadStages();
                                         $uibModalInstance.dismiss('cancel');
-                                    }).finally(function () {
-                                    $scope.isDisableArchiveButton = false;
-                                    $scope.archiveButtonText = "归档";
-                                });
+                                    }).finally(function() {
+                                        $scope.isDisableArchiveButton = false;
+                                        $scope.archiveButtonText = "归档";
+                                    });
                             };
-                        }],
+                        }
+                    ],
                     size: 'mid'
                 });
             };
--- kanban/component/stage/directives/stagesDirective.js
+++ kanban/component/stage/directives/stagesDirective.js
@@ -1,22 +1,22 @@
 /**
  * Created by xubt on 4/29/16.
  */
-kanbanApp.directive('stages', function () {
+kanbanApp.directive('stages', function() {
     return {
         restrict: 'E',
         templateUrl: 'component/stage/partials/stages.html',
         replace: true,
-        controller: ['$scope', 'boardsService', 'stagesServices', 'localStorageService', '$uibModal', 'usersService', 'timerMessageService', 'advancedFilterFactory', function ($scope, boardsService, stagesServices, localStorageService, $uibModal, usersService, timerMessageService, advancedFilterFactory) {
-            $scope.loadSprint = function () {
-                boardsService.loadActiveSprint($scope.board._links.activeSprint.href).then(function (_sprint) {
+        controller: ['$scope', 'boardsService', 'stagesServices', 'localStorageService', '$uibModal', 'usersService', 'timerMessageService', 'advancedFilterFactory', function($scope, boardsService, stagesServices, localStorageService, $uibModal, usersService, timerMessageService, advancedFilterFactory) {
+            $scope.loadSprint = function() {
+                boardsService.loadActiveSprint($scope.board._links.activeSprint.href).then(function(_sprint) {
                     $scope.sprint = _sprint;
-                }, function (_rejection) {
+                }, function(_rejection) {
                     if (_rejection.status === 404) {
                         $scope.noActiveSprintWasFound = true;
                     }
                 });
             };
-            $scope.loadStages = function (_activeViewType, _typeName) {
+            $scope.loadStages = function(_activeViewType, _typeName) {
                 var currentBoard = localStorageService.get("currentBoard");
                 var snapshotLink = _activeViewType === undefined ? currentBoard._links.sprintViewSnapshot.href : currentBoard._links[_activeViewType].href;
                 $scope.typeName = _typeName === undefined ? "迭代视图" : _typeName;
@@ -24,16 +24,17 @@ kanbanApp.directive('stages', function () {
 
                 var boardPromise = boardsService.loadBoardByLink(snapshotLink);
                 $scope.loadingInstance = timerMessageService.loading($scope.typeName + "加载中..");
-                boardPromise.then(function (_board) {
+                boardPromise.then(function(_board) {
                     $scope.board = _board;
                     $scope.loadSprint();
                     $scope.stages = _board.stages.stages;
+                    var currentScope = $scope;
                     timerMessageService.message("最后渲染中...");
                     $scope.stageSortableOptions = {
                         connectWith: ".stage-item",
                         opacity: 0.5,
                         placeholder: "stage-drag-placeholder",
-                        stop: function (e, ui) {
+                        stop: function(e, ui) {
                             if (ui.item.sortable.droptarget === undefined) {
                                 return;
                             }
@@ -41,99 +42,101 @@ kanbanApp.directive('stages', function () {
                             for (var index in stages) {
                                 stages[index].sortNumber = index;
                             }
-                            var sortNumbersLink = JSON.parse(ui.item.sortable.source.attr("boardClone")).stages._links.sortNumbers.href;
+                            var sortNumbersLink = currentScope.board.stages._links.sortNumbers.href;
                             stagesServices.resort(stages, sortNumbersLink);
                         }
                     };
-                }).finally(function () {
+                }).finally(function() {
                     timerMessageService.close($scope.loadingInstance);
                 });
             };
             $scope.loadStages();
-            $scope.closeLoading = function () {
+            $scope.closeLoading = function() {
                 timerMessageService.close($scope.loadingInstance);
             };
             var currentScope = $scope;
-            $scope.openSprintDialog = function () {
+            $scope.openSprintDialog = function() {
                 $uibModal.open({
                     animation: true,
                     templateUrl: 'component/board/partials/sprint-configuration.html',
                     controller: ['$scope', 'boardsService', 'timerMessageService',
-                        function ($scope, boardsService, timerMessageService) {
-                            $scope.sprint = {startTime: moment(), endTime: moment(), sprintName: ""};
+                        function($scope, boardsService, timerMessageService) {
+                            $scope.sprint = { startTime: moment(), endTime: moment(), sprintName: "" };
                             if (currentScope.sprint !== undefined) {
                                 $scope.sprint = currentScope.sprint;
                             }
                             $scope.sprintSaveButtonText = "保存";
                             $scope.isDisableSprintSaveButton = false;
-                            $scope.saveSprint = function () {
+                            $scope.saveSprint = function() {
                                 $scope.isDisableBoardSaveButton = true;
-                                boardsService.saveSprint($scope.sprint, currentScope.board._links.sprints.href).then(function (_sprint) {
+                                boardsService.saveSprint($scope.sprint, currentScope.board._links.sprints.href).then(function(_sprint) {
                                     currentScope.sprint = _sprint;
                                     timerMessageService.message("迭代信息设置成功。");
-                                }).finally(function () {
+                                }).finally(function() {
                                     $scope.isDisableBoardSaveButton = false;
                                 });
                             };
-                            $scope.$watch('sprint', function (newValue, oldValue) {
+                            $scope.$watch('sprint', function(newValue, oldValue) {
                                 if (oldValue === newValue) {
                                     return;
                                 }
                                 if ($scope.sprint.startTime.isAfter($scope.sprint.endTime)) {
                                     $scope.showStartTimeIsAfterEndTime = true;
                                     $scope.isDisableSprintSaveButton = true;
-                                }
-                                else {
+                                } else {
                                     $scope.showStartTimeIsAfterEndTime = false;
                                     $scope.isDisableSprintSaveButton = false;
                                 }
                             });
-                        }],
+                        }
+                    ],
                     size: 'sprint',
                     backdrop: "static"
                 });
             };
-            $scope.openSprintCompetitionDialog = function () {
+            $scope.openSprintCompetitionDialog = function() {
                 $uibModal.open({
                     templateUrl: 'component/board/partials/sprint-competition-confirm-dialog.html',
                     controller: ['$scope', 'boardsService', 'timerMessageService', '$uibModalInstance',
-                        function ($scope, boardsService, timerMessageService, $uibModalInstance) {
+                        function($scope, boardsService, timerMessageService, $uibModalInstance) {
                             $scope.title = '迭代完成前确认';
                             $scope.message = "确认本次迭代已经完成？一经操作后不可撤销，请谨慎操作。";
                             $scope.sprintCompetitionButtonText = "完成迭代";
                             $scope.isDisableSprintCompetitionButton = false;
-                            $scope.ok = function () {
+                            $scope.ok = function() {
                                 $scope.loadingInstance = timerMessageService.loading();
                                 $scope.sprintCompetitionButtonText = "归档中..";
                                 $scope.isDisableSprintCompetitionButton = true;
                                 currentScope.sprint.status = 2;
-                                boardsService.saveSprint(currentScope.sprint, currentScope.board._links.sprints.href).then(function () {
+                                boardsService.saveSprint(currentScope.sprint, currentScope.board._links.sprints.href).then(function() {
                                     currentScope.sprint = undefined;
                                     currentScope.loadStages();
                                     timerMessageService.message("本次迭代已经完成，卡片已经归档。");
-                                }).finally(function () {
+                                }).finally(function() {
                                     timerMessageService.close($scope.loadingInstance);
                                     $scope.sprintCompetitionButtonText = "完成迭代";
                                     $scope.isDisableSprintCompetitionButton = false;
                                 });
                                 $uibModalInstance.close();
                             };
-                            $scope.cancel = function () {
+                            $scope.cancel = function() {
                                 $uibModalInstance.dismiss('cancel');
                             };
-                        }],
+                        }
+                    ],
                     size: 'mid',
                     backdrop: "static"
                 });
             };
-            $scope.openPagesDialog = function () {
+            $scope.openPagesDialog = function() {
                 $uibModal.open({
                     animation: true,
                     templateUrl: 'component/page/partials/pages-dialog.html',
                     controller: ['$scope', 'boardsService', 'timerMessageService',
-                        function ($scope) {
+                        function($scope) {
                             $scope.board = currentScope.board;
-                        }],
+                        }
+                    ],
                     size: 'pages',
                     backdrop: "static"
                 });
