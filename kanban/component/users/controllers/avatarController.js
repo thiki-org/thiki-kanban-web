c2ad601ff4dbb6921eb60fe48ee6894fe58e6e22
skytao <skytao1991@gmail.com>
2017-02-20 21:11:00 +0800

    Optimize card tag creation.

--- kanban/component/users/controllers/avatarController.js
+++ kanban/component/users/controllers/avatarController.js
@@ -3,16 +3,16 @@
  */
 
 kanbanApp.controller('avatarController', ['$scope', '$location', '$q', 'publicKeyServices', 'loginService', 'localStorageService', '$uibModal', 'notificationsService', 'usersService', '$interval', 'timerMessageService', '$rootScope',
-    function ($scope, $location, $q, publicKeyServices, loginService, localStorageService, $uibModal, notificationsService, usersService, $interval, timerMessageService, $rootScope) {
+    function($scope, $location, $q, publicKeyServices, loginService, localStorageService, $uibModal, notificationsService, usersService, $interval, timerMessageService, $rootScope) {
         $scope.title = '上传头像';
         $scope.uploadButtonText = "上传";
         $scope.isDisableUploadButton = false;
-        $scope.ok = function () {
+        $scope.ok = function() {
             localStorageService.clearAll();
             $uibModalInstance.close();
             $location.path("/welcome");
         };
-        $scope.cancel = function () {
+        $scope.cancel = function() {
             $uibModalInstance.dismiss('cancel');
         };
 
@@ -20,18 +20,19 @@ kanbanApp.controller('avatarController', ['$scope', '$location', '$q', 'publicKe
         $scope.cropper.sourceImage = null;
         $scope.cropper.croppedImage = null;
 
-        $scope.uploadAvatar = function () {
+        $scope.uploadAvatar = function() {
             $scope.uploadButtonText = "上传中..";
             $scope.isDisableUploadButton = true;
             var avatarLink = usersService.getCurrentUserProfile()._links.avatar.href;
             var avatarPromise = usersService.uploadAvatar(dataURItoBlob($scope.cropper.croppedImage), avatarLink);
-            avatarPromise.then(function () {
+            avatarPromise.then(function() {
                 timerMessageService.message("头像设置成功。");
                 $rootScope.avatar = $scope.cropper.croppedImage;
-            }).finally(function () {
+            }).finally(function() {
                 $scope.uploadButtonText = "上传";
                 $scope.isDisableUploadButton = false;
                 $uibModalInstance.dismiss('cancel');
             });
         };
-    }]);
+    }
+]);
