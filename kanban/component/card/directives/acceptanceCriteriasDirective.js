e5f649d2aad419381f5a1aba74295d666a7c71f3
skytao <skytao1991@gmail.com>
2017-02-22 13:56:15 +0800

    Fix acceptanceCriteris resort.

--- kanban/component/card/directives/acceptanceCriteriasDirective.js
+++ kanban/component/card/directives/acceptanceCriteriasDirective.js
@@ -1,7 +1,7 @@
 /**
  * Created by xubt on 10/18/16.
  */
-kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
+kanbanApp.directive('acceptanceCriterias', function($uibModal) {
     return {
         restrict: 'E',
         templateUrl: 'component/card/partials/acceptanceCriterias.html',
@@ -11,11 +11,11 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
             card: '=',
             stage: '='
         },
-        controller: ['$scope', 'localStorageService', 'acceptanceCriteriaService', 'timerMessageService', function ($scope, localStorageService, acceptanceCriteriaService, timerMessageService) {
+        controller: ['$scope', 'localStorageService', 'acceptanceCriteriaService', 'timerMessageService', '$filter', function($scope, localStorageService, acceptanceCriteriaService, timerMessageService, $filter) {
             $scope.acceptanceCriteriaSaveButton = "保存";
             $scope.isShowAcceptanceCriteriaForm = false;
 
-            $scope.updateAcceptanceCriteriasCount = function () {
+            $scope.updateAcceptanceCriteriasCount = function() {
                 var finishedAcceptanceCriteriasCount = 0;
                 for (var index in $scope.acceptanceCriterias) {
                     if ($scope.acceptanceCriterias[index].finished === true) {
@@ -28,7 +28,7 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
                 $scope.$parent.acceptanceCriteriasCount = $scope.acceptanceCriterias.length;
             };
 
-            $scope.updateAcceptanceCriteria = function (__acceptanceCriteria) {
+            $scope.updateAcceptanceCriteria = function(__acceptanceCriteria) {
                 for (var index in $scope.acceptanceCriterias) {
                     if ($scope.acceptanceCriterias[index].id === __acceptanceCriteria.id) {
                         $scope.acceptanceCriterias[index] = __acceptanceCriteria;
@@ -38,30 +38,29 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
                 }
             };
 
-            $scope.deleteAcceptanceCriteria = function (__acceptanceCriteria) {
+            $scope.deleteAcceptanceCriteria = function(__acceptanceCriteria) {
                 var index = $scope.card.acceptanceCriterias.acceptanceCriterias.indexOf(__acceptanceCriteria);
                 $scope.card.acceptanceCriterias.acceptanceCriterias.splice(index, 1);
                 $scope.updateAcceptanceCriteriasCount();
             };
 
-            $scope.loadAcceptanceCriterias = function () {
+            $scope.loadAcceptanceCriterias = function() {
                 $scope.acceptanceCriterias = $scope.card.acceptanceCriterias === undefined ? [] : $scope.card.acceptanceCriterias.acceptanceCriterias;
+                $scope.acceptanceCriterias = $filter('orderBy')($scope.acceptanceCriterias, 'sortNumber');
                 $scope.$parent.acceptanceCriteriasCount = $scope.acceptanceCriterias.length;
                 $scope.updateAcceptanceCriteriasCount();
                 var currentScope = $scope;
                 $scope.acceptanceCriteriasSortableOptions = {
                     connectWith: ".acceptanceCriteria",
                     placeholder: "acceptanceCriteria-drag-placeholder",
-                    stop: function (e, ui) {
+                    stop: function(e, ui) {
                         var loadingInstance = timerMessageService.loading();
                         var acceptanceCriterias = ui.item.sortable.sourceModel;
                         for (var index in acceptanceCriterias) {
                             acceptanceCriterias[index].sortNumber = index;
                         }
                         var sortNumbersLink = currentScope.card.acceptanceCriterias._links.sortNumbers.href;
-                        acceptanceCriteriaService.resort(acceptanceCriterias, sortNumbersLink).then(function (_acceptanceCriterias) {
-                            currentScope.card.acceptanceCriterias = _acceptanceCriterias;
-                            currentScope.acceptanceCriterias = _acceptanceCriterias.acceptanceCriterias;
+                        acceptanceCriteriaService.resort(acceptanceCriterias, sortNumbersLink).then(function(_acceptanceCriterias) {
                             timerMessageService.close(loadingInstance);
                         });
                     }
@@ -70,40 +69,40 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
             $scope.loadAcceptanceCriterias();
             $scope.isShowAcceptanceCriteriaCreationButton = true;
             $scope.isDisableAcceptanceCriteriaSaveButton = false;
-            $scope.openAcceptanceCriteriaCreationForm = function () {
+            $scope.openAcceptanceCriteriaCreationForm = function() {
                 $scope.isShowAcceptanceCriteriaForm = true;
                 $scope.isShowAcceptanceCriteriaCreationButton = false;
             };
 
-            $scope.addAcceptanceCriteriaCreation = function () {
+            $scope.addAcceptanceCriteriaCreation = function() {
                 $scope.isShowAcceptanceCriteriaCreationButton = false;
                 $scope.isDisableAcceptanceCriteriaSaveButton = true;
                 $scope.acceptanceCriteriaSaveButton = "保存中..";
 
-                var acceptanceCriteria = {summary: $scope.acceptanceCriteriaSummary};
+                var acceptanceCriteria = { summary: $scope.acceptanceCriteriaSummary };
                 var acceptanceCriteriaPromise = acceptanceCriteriaService.create(acceptanceCriteria, $scope.card._links.acceptanceCriterias.href);
-                acceptanceCriteriaPromise.then(function (_acceptanceCriteria) {
+                acceptanceCriteriaPromise.then(function(_acceptanceCriteria) {
                     $scope.acceptanceCriterias.push(_acceptanceCriteria);
-                    $scope.card.acceptanceCriterias = $scope.card.acceptanceCriterias === undefined ? {acceptanceCriterias: []} : $scope.card.acceptanceCriterias;
+                    $scope.card.acceptanceCriterias = $scope.card.acceptanceCriterias === undefined ? { acceptanceCriterias: [] } : $scope.card.acceptanceCriterias;
                     $scope.card.acceptanceCriterias.acceptanceCriterias = $scope.acceptanceCriterias;
                     $scope.card.totalAcceptanceCriteriasCount = $scope.acceptanceCriterias.length;
                     $scope.isShowAcceptanceCriteriaForm = false;
                     $scope.isShowAcceptanceCriteriaCreationButton = true;
                     $scope.acceptanceCriteriaSummary = "";
                     $scope.updateAcceptanceCriteriasCount();
-                }).finally(function () {
+                }).finally(function() {
                     $scope.acceptanceCriteriaSaveButton = "保存";
                     $scope.isDisableAcceptanceCriteriaSaveButton = false;
                 });
             };
 
-            $scope.cancelCreateAcceptanceCriteria = function () {
+            $scope.cancelCreateAcceptanceCriteria = function() {
                 $scope.isShowAcceptanceCriteriaForm = false;
                 $scope.acceptanceCriteriaSummary = "";
                 $scope.isShowAcceptanceCriteriaCreationButton = true;
             };
 
-            $scope.$watch('card.finishedAcceptanceCriteriasCount', function (newValue, oldValue) {
+            $scope.$watch('card.finishedAcceptanceCriteriasCount', function(newValue, oldValue) {
                 if (oldValue === newValue) {
                     return;
                 }
@@ -114,15 +113,14 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
                         controller: [
                             '$scope',
                             '$uibModalInstance', '$interval',
-                            function ($scope, $uibModalInstance, $interval) {
+                            function($scope, $uibModalInstance, $interval) {
                                 $scope.timerMessage = "3秒后自动关闭";
                                 var count = 2;
-                                $scope.timer = $interval(function () {
+                                $scope.timer = $interval(function() {
                                     if (count === 0) {
                                         $interval.cancel($scope.timer);
                                         $uibModalInstance.dismiss('cancel');
-                                    }
-                                    else {
+                                    } else {
                                         $scope.timerMessage = count + "秒后自动关闭";
                                     }
                                     count--;
@@ -137,5 +135,3 @@ kanbanApp.directive('acceptanceCriterias', function ($uibModal) {
         }]
     };
 });
-
-
