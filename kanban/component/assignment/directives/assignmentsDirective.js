c24a53bbbbf1732a739decee2cdfe23bdf06f0e1
skytao <skytao1991@gmail.com>
2017-02-24 16:20:06 +0800

    Optimize child card representation.

--- kanban/component/assignment/directives/assignmentsDirective.js
+++ kanban/component/assignment/directives/assignmentsDirective.js
@@ -9,10 +9,39 @@ kanbanApp.directive('assignments', function() {
         controller: ['$scope', 'assignmentServices', 'timerMessageService', function($scope, assignmentServices, timerMessageService) {
             $scope.loadAssignments = function() {
                 var thisScope = $scope;
-                $scope.assignments = $scope.card.assignments === undefined ? [] : $scope.card.assignmentsNode.assignments;
+                var assignments = $scope.card.assignmentsNode === undefined ? [] : $scope.card.assignmentsNode.assignments;
+
                 thisScope.initAssignmentStatus();
+                if ($scope.card.child !== undefined) {
+                    for (var index in $scope.card.child.cards) {
+                        if ($scope.card.child.cards[index] !== undefined && $scope.card.child.cards[index].assignmentsNode !== undefined) {
+                            assignments = assignments.concat($scope.card.child.cards[index].assignmentsNode.assignments);
+                        }
+                    }
+                }
+                var filteredAssignments = [];
+                for (var index in assignments) {
+                    var isFilteredAssignmentsHasAlreadyIncluded = false;
+                    for (var filteredIndex in filteredAssignments) {
+                        if (filteredAssignments[filteredIndex] !== undefined && filteredAssignments[filteredIndex].assignee === assignments[index].assignee) {
+                            isFilteredAssignmentsHasAlreadyIncluded = true;
+                        }
+                    }
+                    if (!isFilteredAssignmentsHasAlreadyIncluded) {
+                        filteredAssignments.push(assignments[index]);
+                    }
+                }
+                $scope.assignments = filteredAssignments;
             };
             $scope.loadAssignments();
+            $scope.$watch(function() {
+                return $scope.card.child;
+            }, function(newValue, oldValue) {
+                if (newValue === oldValue) {
+                    return;
+                }
+                $scope.loadAssignments();
+            }, true);
         }]
     };
 });
