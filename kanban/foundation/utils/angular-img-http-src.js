ad313c0972283ab5b769b4bc304fd6ef9795b5fd
skytao <skytao1991@gmail.com>
2017-03-15 20:17:11 +0800

    Optimize image cache.

--- kanban/foundation/utils/angular-img-http-src.js
+++ kanban/foundation/utils/angular-img-http-src.js
@@ -1,7 +1,7 @@
 /**
  * Created by xubt on 26/02/2017.
  */
-kanbanApp.directive('httpSrc', ['$http', function ($http) {
+kanbanApp.directive('httpSrc', ['$http', 'localStorageService', function ($http, localStorageService) {
     return {
         // do not share scope with sibling img tags and parent
         // (prevent show same images on img tag)
@@ -23,26 +23,38 @@ kanbanApp.directive('httpSrc', ['$http', function ($http) {
 
             attrs.$observe('httpSrc', function (url) {
                 revokeObjectURL();
-                console.log(url);
                 if (url === undefined || url === null || url === "") {
                     return;
                 }
                 if (url && url.indexOf('data:') === 0) {
                     $scope.objectURL = url;
                 } else if (url) {
+                    if (kanbanApp.isCacheUrl(url)) {
+                        var cachedImageResponseDataString = localStorageService.get(url);
+                        if (localStorageService.get(url) !== null) {
+                            console.log("Load image file from local cache:" + url);
+                            var bufferString = stringToArrayBuffer(cachedImageResponseDataString);
+                            var blob = new Blob(
+                                [bufferString],
+                                {type: "image/*"}
+                            );
+                            $scope.objectURL = URL.createObjectURL(blob);
+                            return;
+                        }
+                    }
                     $http.get(url, {
                         responseType: 'arraybuffer',
                         headers: {
                             'accept': 'image/webp,image/*,*/*;q=0.8'
                         }
-                    })
-                        .then(function (response) {
-                            var blob = new Blob(
-                                [response.data],
-                                {type: response.headers('Content-Type')}
-                            );
-                            $scope.objectURL = URL.createObjectURL(blob);
-                        });
+                    }).then(function (response) {
+                        var blob = new Blob(
+                            [response.data],
+                            {type: "image/*"}
+                        );
+                        $scope.objectURL = URL.createObjectURL(blob);
+                        localStorageService.set(url, arrayBufferToString(response.data));
+                    });
                 }
             });
         }
