448b233e5d02c14b3cff666eb889ba6ce6053fbf
skytao <skytao1991@gmail.com>
2017-03-15 20:17:42 +0800

    Optimize avatar representation.

--- kanban/component/users/directives/nickNameDirective.js
+++ kanban/component/users/directives/nickNameDirective.js
@@ -4,7 +4,7 @@
 
 kanbanApp.directive('nickName', function () {
     return {
-        restrict: 'E',
+        restrict: 'A,E,C',
         templateUrl: 'component/users/partials/nick-name.html',
         replace: true,
         transclude: true,
@@ -14,6 +14,22 @@ kanbanApp.directive('nickName', function () {
         controller: ['$scope', 'usersService', function ($scope, usersService) {
             usersService.loadProfile($scope.profileLink).then(function (_profile) {
                 $scope.nickName = _profile.nickName;
+                $scope.$parent.profile = _profile;
+            });
+        }]
+    };
+});
+
+kanbanApp.directive('profilePlain', function () {
+    return {
+        restrict: 'A,E,C',
+        scope: {
+            profileLink: '='
+        },
+        controller: ['$scope', 'usersService', function ($scope, usersService) {
+            usersService.loadProfile($scope.profileLink).then(function (_profile) {
+                $scope.profile = _profile;
+                $scope.$parent.profile = _profile;
             });
         }]
     };
