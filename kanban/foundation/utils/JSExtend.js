a3023c074de861587a1a40d70cfcc6e7be1daa69
skytao <skytao1991@gmail.com>
2017-03-19 12:34:24 +0800

    Refactor code.

---
+++ kanban/foundation/utils/JSExtend.js
@@ -0,0 +1,57 @@
+/**
+ * Created by xubt on 18/02/2017.
+ */
+Array.prototype.indexOf || (Array.prototype.indexOf = function (d, e) {
+    var a;
+    if (null == this) throw new TypeError('"this" is null or not defined');
+    var c = Object(this),
+        b = c.length >>> 0;
+    if (0 === b) return -1;
+    a = +e || 0;
+    Infinity === Math.abs(a) && (a = 0);
+    if (a >= b) return -1;
+    for (a = Math.max(0 <= a ? a : b - Math.abs(a), 0); a < b;) {
+        if (a in c && c[a] === d) return a;
+        a++
+    }
+    return -1
+});
+
+String.prototype.replaceAll = function (search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
+
+function dataURItoBlob(dataURI) {
+    var binary = atob(dataURI.split(',')[1]);
+    var array = [];
+    for (var i = 0; i < binary.length; i++) {
+        array.push(binary.charCodeAt(i));
+    }
+    return new Blob([new Uint8Array(array)], {type: 'image/jpeg'});
+}
+function arrayBufferToString(buffer) {
+    var bufView = new Uint16Array(buffer);
+    var length = bufView.length;
+    var result = '';
+    var addition = Math.pow(2, 16) - 1;
+
+    for (var i = 0; i < length; i += addition) {
+
+        if (i + addition > length) {
+            addition = length - i;
+        }
+        result += String.fromCharCode.apply(null, bufView.subarray(i, i + addition));
+    }
+
+    return result;
+}
+
+function stringToArrayBuffer(str) {
+    var buf = new ArrayBuffer(str.length * 2); // 2 bytes for each char
+    var bufView = new Uint16Array(buf);
+    for (var i = 0, strLen = str.length; i < strLen; i++) {
+        bufView[i] = str.charCodeAt(i);
+    }
+    return buf;
+}
